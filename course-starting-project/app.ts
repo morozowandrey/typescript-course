6c2950fbf5f5bcd5ceee6da5f615d4c2bee28f58
Andrii Morozov <andrey940@gmail.com>
2022-04-12 00:06:33 +0300

    number type playground

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,1 +1,9 @@
-console.log('razbimbim bombas');
+function sum(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+const num1 = 5;
+const num2 = 3.3;
+
+const res = sum(num1, num2);
+console.log(res);
